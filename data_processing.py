4c5f590946785040332eb543f0ac0dd90fe93c2f
Eric <ericchavez00@gmail.com>
2025-03-31 14:21:53 -0500

    Specify which columns to record to cut down on file size. Also add the text records to a gitignore

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,21 @@
 import requests, time
+currentTime = 0
 while True:
     data = requests.get('https://gbfs.lyft.com/gbfs/2.3/bkn/en/station_status.json').json()
-    #write = open("test.txt","a")
+    records = open("records.txt","a")
     for station in data['data']['stations']:
+        #Station id corresponding to 88th St and Park Ave
         if station['station_id']=="66dde103-0aca-11e7-82f6-3863bb44ef7c":
-            print(station)
-            #write.write(","+str(station))
+            if station['last_reported'] != currentTime:
+                currentTime = station['last_reported']
+                stationInformation = {}
+                stationInformation['station_id'] = "66dde103-0aca-11e7-82f6-3863bb44ef7c"
+                stationInformation['last_reported'] = currentTime 
+                stationInformation['is_installed'] = station['is_installed']
+                stationInformation['is_renting'] = station['is_renting']
+                stationInformation['is_returning'] = station['is_returning']
+                stationInformation['num_docks_available'] = station['num_docks_available']
+                stationInformation['num_ebikes_available'] = station['num_ebikes_available']
+                stationInformation['num_bikes_available'] = station['num_bikes_available']
+                records.write(","+str(stationInformation))
     time.sleep(60)
