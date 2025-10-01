1c857500f67c03aad7251dd3e46151e209068566
Eric <ericchavez00@gmail.com>
2025-10-01 12:48:17 -0500

    updated webscraper to save entire bike station json instead of one station

--- data_scraper.py
+++ data_scraper.py
@@ -1,23 +1,15 @@
 import requests, time
-currentTime = 0
+lastReportedTime = {}
 while True:
     response = requests.get('https://gbfs.lyft.com/gbfs/2.3/bkn/en/station_status.json')
     if response.ok:
         data=response.json()
         records = open("records.txt","a")
         for station in data['data']['stations']:
-            #Station id corresponding to 88th St and Park Ave
-            if station['station_id']=="66dde103-0aca-11e7-82f6-3863bb44ef7c":
-                if station['last_reported'] != currentTime:
-                    currentTime = station['last_reported']
-                    stationInformation = {}
-                    stationInformation["station_id"] = "66dde103-0aca-11e7-82f6-3863bb44ef7c"
-                    stationInformation["last_reported"] = currentTime 
-                    stationInformation["is_installed"] = station['is_installed']
-                    stationInformation["is_renting"] = station['is_renting']
-                    stationInformation["is_returning"] = station['is_returning']
-                    stationInformation["num_docks_available"] = station['num_docks_available']
-                    stationInformation["num_ebikes_available"] = station['num_ebikes_available']
-                    stationInformation["num_bikes_available"] = station['num_bikes_available']
-                    records.write("\n"+str(stationInformation))
+            stationID = station['station_id']
+            if stationID not in lastReportedTime:
+                lastReportedTime[stationID] = station['last_reported']
+            if station['last_reported'] != lastReportedTime[stationID]:
+                lastReportedTime[stationID] = station['last_reported']
+                records.write("\n"+str(station))
     time.sleep(60)
