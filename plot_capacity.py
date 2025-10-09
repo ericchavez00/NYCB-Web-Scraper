1806e12ce02c9dd2be20a31146fd18a13a06717e
Eric <ericchavez00@gmail.com>
2025-10-09 11:42:10 -0500

    added number of disabled docks and bikes to capacity plot

--- plot_capacity.py
+++ plot_capacity.py
@@ -7,10 +7,12 @@ with open("records.txt", "r") as file:
     data = file.read()   
 rows=data.split('\n')
 for row in rows:
+    row = row.replace("'", '"')
     row =json.loads(row)
-    maxCapacity = row['num_docks_available']+row['num_bikes_available']
-    timestamps.append(row['last_reported'])
-    maxCapacities.append(maxCapacity)
+    if row['station_id'] == '0f45bcf6-7028-4584-a51e-4129847dbebc':
+        maxCapacity = row['num_docks_available']+row['num_bikes_available']+row['num_docks_disabled']+row['num_bikes_disabled']
+        timestamps.append(row['last_reported'])
+        maxCapacities.append(maxCapacity)
 plt.scatter(timestamps,maxCapacities)
 plt.plot(timestamps, maxCapacities, color='red', label='Connecting Line')
 plt.show()
